cc7d9e30238a983c5b30d5e513aca8d0027e6ad2
Jaygan <dks3752@naver.com>
2022-07-03 10:43:03 +0900

    more interaction

--- UWPTest/MainPage.xaml.cs
+++ UWPTest/MainPage.xaml.cs
@@ -30,9 +30,9 @@ namespace UWPTest
     {
         private Random random;
         public Vector2 position;
-        public float maxSpeedPerSecond = 10.0f;
-        public float randomnessPower = 0.1f;
-        public float randomnessAngle = 0.4f;
+        public float maxSpeed = 0.1f;
+        public float randomnessPower = 0.2f;
+        public float randomnessAngle = 0.1f;
         private float power = 0;
         private float angle;
         public Color col;
@@ -44,36 +44,48 @@ namespace UWPTest
             angle = (float)(ra.NextDouble() * (float)(Math.PI * 2.0));
             random = ra;
         }
-        public void GenerateRandomness(Vector2 boundary)
+        public void GenerateRandomness(Vector2 boundary1, Vector2 boundary2, Vector2 externalPowerVector)
         {
-            // 랜덤한 힘/각도를 생성
+            // generate random power and angle amount
             float randomPower = (float)random.NextDouble() - 0.5f;
             float randomAngle = (float)random.NextDouble() - 0.5f;
 
-            // 현재의 힘/각도에 랜덤인자를 더함
-            power += Math.Min(randomPower * randomnessPower, maxSpeedPerSecond);
+            // add generated random values to current power and angle
+            power += Math.Min(randomPower * randomnessPower, maxSpeed);
             angle += randomAngle * randomnessAngle;
 
-            // 현재의 힘/각도로 움직일 X, Y 거리 계산
-            float amountX = power * (float)Math.Sin(angle);
-            float amountY = power * (float)Math.Cos(angle);
+            // calculate amount of x/y movement with current power and angle
+            float amountX = power * (float)Math.Cos(angle) + externalPowerVector.X;
+            float amountY = power * (float)Math.Sin(angle) + externalPowerVector.Y;
 
+            // calculate next position, and check for boundary violation
             Vector2 newPosition = position;
             newPosition.X += amountX;
             newPosition.Y += amountY;
-
-            if(newPosition.X < 0.0f || newPosition.X > boundary.X 
-                || newPosition.Y < 0.0f || newPosition.Y > boundary.Y)
+            if(newPosition.X < boundary1.X || newPosition.X > boundary2.X 
+                || newPosition.Y < boundary1.Y || newPosition.Y > boundary2.Y)
             {
+                // out of boundary, flip angle and kick back the almond
                 angle -= (float)Math.PI;
-                amountX = power * (float)Math.Sin(angle);
-                amountY = power * (float)Math.Cos(angle);
+
+                amountX = power * (float)Math.Cos(angle);
+                amountY = power * (float)Math.Sin(angle);
                 position.X += amountX;
                 position.Y += amountY;
+
+                // check if the almond is stuck(mostly due to panning or zoom) outside of the boundary, and summon back to inside
+                if (position.X < boundary1.X)
+                    position.X = boundary1.X + amountX;
+                else if (position.X > boundary2.X)
+                    position.X = boundary2.X + amountX;
+                if (position.Y < boundary1.Y)
+                    position.Y = boundary1.Y + amountY;
+                else if (position.Y > boundary2.Y)
+                    position.Y = boundary2.Y + amountY;
             }
             else
             {
-                // 위치를 변경
+                // update position if there is no violation
                 position = newPosition;
             }
         }
@@ -82,16 +94,14 @@ namespace UWPTest
 
     public sealed partial class MainPage : Page
     {
-        static int count = 10000;
-        public Random rara = new Random();
         Almond[] test;
-        CanvasCachedGeometry[] canvasCachedGeometry = new CanvasCachedGeometry[3];
-        DateTime _lastTime; // marks the beginning the measurement began
-        int _framesRendered; // an increasing count
-        int _fps; // the FPS calculated from the last measurement
+        static int count = 20000; // number of almonds(particles)
+        public Random randomMachine = new Random();
+        CanvasCachedGeometry canvasCachedGeometry;
+
+
         CanvasTextFormat canvasTextFormat = new CanvasTextFormat();
-        Vector2 bobo;
-        Color transW;
+        //Color transW;
         float minScale = 0.005f;
         float targetScale = 1.0f;
         float maxScale = 5.0f;
@@ -115,8 +125,13 @@ namespace UWPTest
             canvasTextFormat.FontSize = 11.0f;
         }
 
+        // variables of fps counter, copy&pasted from stackoverflow; thanks
+        DateTime _lastTime; // marks the beginning the measurement began
+        int _framesRendered; // an increasing count
+        int _fps; // the FPS calculated from the last measurement
         private void CanvasAnimatedControl_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
         {
+            // fps counter, copy&pasted from stackoverflow;
             _framesRendered++;
             if ((DateTime.Now - _lastTime).TotalSeconds >= 1)
             {
@@ -126,6 +141,7 @@ namespace UWPTest
                 _lastTime = DateTime.Now;
             }
 
+            // draw almonds
             using (CanvasCommandList ccl = new CanvasCommandList(sender))
             {
                 using (CanvasDrawingSession cds = ccl.CreateDrawingSession())
@@ -134,16 +150,26 @@ namespace UWPTest
                     
                     for (int i = 0; i < count; i++)
                     {
-                        cds.DrawCachedGeometry(canvasCachedGeometry[1], test[i].position, test[i].col);
+                        cds.DrawCachedGeometry(canvasCachedGeometry, test[i].position, test[i].col);
                     }
                 }
                 args.DrawingSession.DrawImage(ccl);
             }
 
-            if(MainCanvas != null)
+            // debug print session
+            bool debug = true;
+            if (debug && MainCanvas != null)
             {
                 Vector2 topleft = (Vector2.Zero - panning - offset) / targetScale;
                 Vector2 bottomright = topleft + (renderSize / targetScale);
+                Vector2 off;
+                Vector2 sca;
+                off.X = currentMatrix.M31;
+                off.Y = currentMatrix.M32;
+                sca.X = currentMatrix.M11;
+                sca.Y = currentMatrix.M22;
+                Vector2 topleft2 = (Vector2.Zero - off) / sca;
+                Vector2 bottomright2 = topleft + (renderSize / sca);
                 string DebugText =
                     $"FPS: {_fps}\n" +
                     $"PARTICLES: {count}\n" +
@@ -151,47 +177,83 @@ namespace UWPTest
                     $"PANNING_OFFSET: {panning}\n" +
                     $"ZOOMING_OFFSET: {offset}\n" +
                     $"RENDER_SIZE: {renderSize}\n" +
-                    $"TOP_LEFT_: {topleft}\n" +
-                    $"BOTTOM_RIGHT: {bottomright}\n" +
+                    $"TARGET_TOP_LEFT: {topleft}\n" +
+                    $"TARGET_BOTTOM_RIGHT: {bottomright}\n" +
                     $"VIEWPORT_SIZE: {bottomright - topleft}\n" +
-                    $"CURSOR_POSITION: {cursorPos}";
+                    $"CURSOR_POSITION: {cursorPos}\n" +
+                    $"CURRENT_MATRIX: {currentMatrix}\n" +
+                    $"OFF: {off}\n" +
+                    $"SCA: {sca}\n" +
+                    $"CURRENT_TOP_LEFT: {topleft2}\n" +
+                    $"CURRENT_BOTTOM_RIGHT: {bottomright2}";
                 args.DrawingSession.DrawText(DebugText, 0.0f, 0.0f, Colors.White, canvasTextFormat);
             }
         }
 
+        // some random sigmoid function, copy&pasted from stackoverflow
+        public static float Sigmoid(float value)
+        {
+            return 1.0f / (1.0f + (float)Math.Exp(-value));
+        }
         private void MainCanvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
         {
-            for (int i = 0; i < count; i++)
-            {
-                test[i].GenerateRandomness(bobo);
-            }
+            // lerp current render matrix to target matrix
             actualMatrix =
                 Matrix3x2.CreateScale(targetScale, targetScale) *
                 Matrix3x2.CreateTranslation(panning) *
                 Matrix3x2.CreateTranslation(offset);
             currentMatrix = Matrix3x2.Lerp(currentMatrix, actualMatrix, 0.2f);
+
+            for (int i = 0; i < count; i++)
+            {
+                // boundary calculation of current(with lerp) render transform matrix
+                Vector2 off; // current offset vector with lerp
+                off.X = currentMatrix.M31;
+                off.Y = currentMatrix.M32;
+
+                Vector2 sca; // current scale vector with lerp
+                sca.X = currentMatrix.M11;
+                sca.Y = currentMatrix.M22;
+
+                // calculate current boundary with zoom, panning and lerp of matrix
+                Vector2 topleft = (Vector2.Zero - off) / sca;
+                Vector2 bottomright = topleft + (renderSize / sca);
+
+                // decide almond's next speed vector with randomness
+                Vector2 cursorDiff = cursorPos - test[i].position;
+                Vector2 cursorPower;
+                float diffR = cursorDiff.Length();
+                float diffRpow2 = diffR * diffR;
+                float powerConst = 500.0f;
+                cursorPower.X = cursorDiff.X * (powerConst / diffRpow2);
+                cursorPower.Y = cursorDiff.Y * (powerConst / diffRpow2);
+                test[i].GenerateRandomness(topleft, bottomright, -cursorPower);
+            }
         }
 
         private void MainCanvas_CreateResources(CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
         {
+            // creation of circle geometry
             CanvasGeometry geometry1 = CanvasGeometry.CreateCircle(sender, 0.0f, 0.0f, 2.0f);
-            canvasCachedGeometry[1] = CanvasCachedGeometry.CreateFill(geometry1);
+            canvasCachedGeometry = CanvasCachedGeometry.CreateFill(geometry1);
 
-            transW = Color.FromArgb(128, 255, 255, 255);
+            // creation of line color(for the test purpose)
+            //transW = Color.FromArgb(128, 255, 255, 255);
+            
+            // create array of almond, number of almond refers to static int value named 'count'
             test = new Almond[count];
             for (int i = 0; i < count; i++)
             {
-                test[i] = new Almond(rara, 900.0f, 450.0f);
-                test[i].col = Color.FromArgb(255, (byte)rara.Next(255), (byte)rara.Next(255), (byte)rara.Next(255)); 
+                // create new almond, 
+                test[i] = new Almond(randomMachine, 900.0f, 450.0f);
+
+                // give random color to almond
+                test[i].col = Color.FromArgb(255, (byte)randomMachine.Next(255), (byte)randomMachine.Next(255), (byte)randomMachine.Next(255)); 
             }
         }
 
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            for (int i = 0; i < count; i++)
-            {
-                bobo = MainCanvas.RenderSize.ToVector2();
-            }
             renderSize = MainCanvas.RenderSize.ToVector2();
         }
 
@@ -235,9 +297,7 @@ namespace UWPTest
             {
                 Vector2 mousePos = e.GetCurrentPoint(MainCanvas).Position.ToVector2();
                 Vector2 topLeft = (Vector2.Zero - panning - offset) / targetScale;
-                Vector2 bottomRight = topLeft + (renderSize / targetScale);
                 cursorPos = (mousePos / targetScale) + topLeft;
-                Vector2 viewportSize = bottomRight - topLeft;
 
             }
             if (pressed)
